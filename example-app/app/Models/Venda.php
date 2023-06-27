a99d40132ebf34bd6b3154671b1a4d9389d4bdee
Leonardo Padilha Fontana <leonardopf@whbbrasil.com.br>
2023-06-27 08:09:41 -0300

    TrabalhandoModels

--- example-app/app/Models/Venda.php
+++ example-app/app/Models/Venda.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Venda extends Model
 {
     use HasFactory;
+    
+    protected $fillable = [
+        'id',
+        'nome',
+        'descricao',
+        'valor',
+        'quantidade'
+    ];
 }
